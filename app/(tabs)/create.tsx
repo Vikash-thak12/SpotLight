4c0263a145901bbab978b116dcf93891c39175de
Vikash Thakur <thakvikash622@gmail.com>
2025-03-06 12:39:26 +0530

    Installed: Expo-Image-Picker

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function create() {
+export default function CreateScreen() {
   return (
     <View>
-      <Text>create</Text>
+      <Text>Create Post Screen</Text>
     </View>
   )
 }
