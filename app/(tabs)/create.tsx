37ac69795c3972c244b900ea0e11c2348f8e89d3
Vikash Thakur <thakvikash622@gmail.com>
2025-03-06 14:02:22 +0530

    Created: UI for CreatePost

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,10 +1,141 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, ScrollView, KeyboardAvoidingView, Share, ActivityIndicator, Platform, TextInput } from 'react-native'
+import React, { useState } from 'react'
+import { useRouter } from 'expo-router'
+import { useUser } from '@clerk/clerk-expo';
+import { Ionicons } from '@expo/vector-icons';
+import { COLORS } from '@/constants/theme';
+import * as ImagePicker from 'expo-image-picker';
+import { Image } from "expo-image";
+import { style } from '@/styles/create.styles';
 
 export default function CreateScreen() {
+  const router = useRouter();
+  const { user } = useUser();
+
+  const [caption, setCaption] = useState("");
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [isSharing, setIsSharing] = useState(false);
+
+
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: "images", 
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 0.8,
+    });
+    
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    }
+  }
+
+  console.log("Selected Image: ", selectedImage);
+
+
+  if (!selectedImage) {
+    return (
+      <View className='bg-gray-300 flex-1 px-2 py-2'>
+        {/* Header */}
+        <View className='flex-row items-center justify-between bg-gray-500 p-2'>
+          <TouchableOpacity onPress={() => router.back()}>
+            <Ionicons name='arrow-back' size={32} color={COLORS.primary} />
+          </TouchableOpacity>
+          <Text className='font-bold'>New Post</Text>
+          <Text></Text>
+        </View>
+
+        {/* Select Image */}
+        <TouchableOpacity className='flex items-center justify-center mt-4 h-[85%] bg-gray-500' onPress={pickImage}>
+          <Ionicons name='image-outline' size={60} color={COLORS.grey} />
+          <Text>Tap to Select an Image</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
   return (
-    <View>
-      <Text>Create Post Screen</Text>
-    </View>
+    <KeyboardAvoidingView 
+    style={style.container}
+    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    keyboardVerticalOffset={Platform.OS === 'ios' ? 20 : 0}
+    >
+      <View style={style.contentContainer}>
+        {/* Header */}
+        <View className='flex-row items-center justify-between bg-gray-500 p-2'>
+        {/* <View className='flex-row items> */}
+          <TouchableOpacity
+            onPress={() => {
+              setSelectedImage(null);
+              setCaption("");
+            }}
+            disabled={isSharing}
+          >
+            <Ionicons name='close-outline' size={32} color={COLORS.primary} />
+          </TouchableOpacity>
+          <Text className='font-bold'>New Post</Text>
+          <TouchableOpacity
+          // disabled={isSharing || !selectedImage}
+          // onPress={handleShare}
+          >
+            {
+              isSharing ? (
+                <ActivityIndicator size="small" color={COLORS.primary} />
+              ) : (
+                <Text className='font-bold'>Share</Text>
+              )
+            }
+          </TouchableOpacity>
+        </View>
+
+
+        {/* body */}
+        <ScrollView
+          contentContainerStyle={style.scrollContent}
+          bounces={false}
+          keyboardShouldPersistTaps='handled'
+        >
+          <View>
+
+            {/* Image Section */}
+            <View style={style.imageSection}>
+              <Image
+                source={selectedImage}
+                style={{ width: '100%', height: '100%' }}
+                contentFit='cover'
+                transition={200}
+              />
+              <TouchableOpacity style={style.changeImageButton} onPress={pickImage} className='px-4 py-2'>
+                <Ionicons name='image-outline' size={28} color={COLORS.primary} />
+                <Text className='text-white'>Change</Text>
+              </TouchableOpacity>
+            </View>
+
+            {/* Input Section */}
+            <View className='flex-1 p-5'>
+              <View className='flex-row items-start gap-5'>
+                <Image
+                source={user?.imageUrl}
+                style={{ width: 40, height: 40, borderRadius: 20 }}
+                
+                contentFit='cover'
+                transition={200}
+                 />
+                 <TextInput
+                 style={style.captionInput}
+                 className='flex-1 fontsize-16 placeholder:text-gray-500'
+                  value={caption} 
+                  onChangeText={setCaption}
+                  placeholder='Write a caption...'
+                  multiline
+                  editable={!isSharing}
+                  />
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+    </KeyboardAvoidingView>
   )
 }
---
+++ styles/create.styles.ts
@@ -0,0 +1,52 @@
+import { COLORS } from "@/constants/theme";
+import { Dimensions, StyleSheet } from "react-native";
+
+const { width } = Dimensions.get('window');
+
+export const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.background,
+    }, 
+    contentContainer: {
+        flex: 1
+    }, 
+    contentDisabled: {
+        opacity: 0.7
+    }, 
+    shareButton: {
+        paddingHorizontal: 12, 
+        paddingVertical: 6, 
+        minWidth: 60, 
+        alignItems: "center",
+        justifyContent: "center",
+    }, 
+    shareButtonDisabled: {
+        opacity: 0.7
+    }, 
+    scrollContent: {
+        flexGrow: 1
+    }, 
+    imageSection: {
+        width: width, 
+        height: width,
+        backgroundColor: COLORS.surface, 
+        justifyContent: "center",
+        alignItems: "center",
+    }, 
+    changeImageButton: {
+        position: "absolute",
+        bottom: 16, 
+        right: 16,
+        backgroundColor: "black",
+        flexDirection: "row",
+        alignItems: "center",
+        borderRadius: 15,
+        gap: 4,
+    }, 
+    captionInput: {
+        color: COLORS.white, 
+        minHeight: 40, 
+        paddingTop: 8
+    }
+})
