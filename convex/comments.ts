a8f196cbef760857eca4d9c14e0b9f931eb8ac16
Vikash Thakur <thakvikash622@gmail.com>
2025-03-08 20:53:22 +0530

    Created: GetComment Query function

--- convex/comments.ts
+++ convex/comments.ts
@@ -1,5 +1,5 @@
 import { ConvexError, v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { getAuthenticatedUser } from "./users";
 
 export const addComment = mutation({
@@ -10,7 +10,7 @@ export const addComment = mutation({
     handler: async (ctx, args) => {
         const currentUser = await getAuthenticatedUser(ctx);
         const post = await ctx.db.get(args.postId);
-        if(!post) throw new ConvexError("Post not found");
+        if (!post) throw new ConvexError("Post not found");
 
 
         const commentId = await ctx.db.insert("comments", {
@@ -19,23 +19,46 @@ export const addComment = mutation({
             content: args.content,
         })
 
-        await ctx.db.patch(args.postId, { comments: post.comments + 1})
+        await ctx.db.patch(args.postId, { comments: post.comments + 1 })
 
         // update Notification table when the comment is done by another user
-        if(post.userId != currentUser._id){
+        if (post.userId != currentUser._id) {
             await ctx.db.insert("notifications", {
-                receiverId: post.userId, 
-                senderId: currentUser._id, 
-                type: "comment", 
-                postId: args.postId, 
-                commentId, 
+                receiverId: post.userId,
+                senderId: currentUser._id,
+                type: "comment",
+                postId: args.postId,
+                commentId,
             })
         }
 
-        return commentId; 
+        return commentId;
     }
 })
 
-// export const getCommnets = mutation({
+export const getCommnets = query({
+    args: {
+        postId: v.id("posts"),
+    },
+    handler: async (ctx, args) => {
+        // will be getting all the comments for a specific post
+        const comments = await ctx.db.query("comments")
+            .withIndex("by_post", (q) => q.eq("postId", args.postId))
+            .collect();
+        
+        const commentswithInfo = await Promise.all(
+            comments.map(async (comment) => {
+                const user = await ctx.db.get(comment.userId);
+                return {
+                    ...comment,
+                    user: {
+                        fullname: user?.fullname,
+                        image: user?.image,
+                    }
+                }
+            })
+        )
 
-// })
+        return commentswithInfo; 
+    }
+})
