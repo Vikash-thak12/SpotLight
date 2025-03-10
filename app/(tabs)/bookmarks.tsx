e680a701352d89c2626393a08e8891edcefaf540
Vikash Thakur <thakvikash622@gmail.com>
2025-03-11 01:34:54 +0530

    Fixed: Bookmark Screen

--- app/(tabs)/bookmarks.tsx
+++ app/(tabs)/bookmarks.tsx
@@ -1,10 +1,57 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import { useQuery } from 'convex/react'
+import { api } from '@/convex/_generated/api'
+import Loader from '@/components/Loader'
+import { COLORS } from '@/constants/theme'
+import { Image } from 'expo-image'
 
 export default function BookMarks() {
+  const bookmarks = useQuery(api.bookmarks.getBookmarks)
+  if (bookmarks === undefined) return <Loader />
+
+  if (bookmarks.length === 0) return <NoBookmarks />
   return (
-    <View className=''>
-      <Text>BookMarks for your Easiness</Text>
+    <View className='flex-1 bg-black'>
+      <View>
+        <Text className='text-center font-bold py-2 text-2xl' style={{ color: COLORS.primary }}>BookMarks</Text>
+      </View>
+      <ScrollView
+        horizontal
+        contentContainerStyle={{
+          padding: 8,
+          flexDirection: "row",
+          flexWrap: "wrap"
+        }}
+      >
+
+        {
+          bookmarks.map((post) => {
+            if (!post) return null;
+            return (
+              <View key={post._id} style={{ width: "33.33%"}}>
+                <Image
+                  source={post.imageUrl}
+                  style={{ width: 100, height: 100, aspectRatio: 1 }}
+                  contentFit='cover'
+                  transition={200}
+                  cachePolicy="memory-disk"
+                />
+              </View>
+            )
+          })
+        }
+      </ScrollView>
+    </View>
+  )
+}
+
+const NoBookmarks = () => {
+  return (
+    <View className='flex-1 items-center justify-center bg-black'>
+      <Text
+        style={{ color: COLORS.primary }}
+        className='font-bold text-2xl'>No BookMarks</Text>
     </View>
   )
 }
