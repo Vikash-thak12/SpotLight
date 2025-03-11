5a64d8abfa75fd337aa29d247bcf50e73360e822
Vikash Thakur <thakvikash622@gmail.com>
2025-03-12 01:03:18 +0530

    Fixed: Image modal

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,99 +1,155 @@
-import Loader from '@/components/Loader'
-import { COLORS } from '@/constants/theme'
-import { api } from '@/convex/_generated/api'
-import { useUser } from '@clerk/clerk-expo'
-import { Ionicons } from '@expo/vector-icons'
-import { useQuery } from 'convex/react'
-import { Image } from 'expo-image'
-import React, { useEffect, useState } from 'react'
-import { Modal, Text, TouchableOpacity, View } from "react-native"
+import Loader from "@/components/Loader";
+import { COLORS } from "@/constants/theme";
+import { api } from "@/convex/_generated/api";
+import { Doc } from "@/convex/_generated/dataModel";
+import { styles } from "@/styles/profile.styles";
+import { useAuth } from "@clerk/clerk-react"
+import { Ionicons } from "@expo/vector-icons";
+import { useQuery } from "convex/react";
+import { Image } from "expo-image";
+import { useState } from "react";
+import { FlatList, Modal, ScrollView, Text, TouchableOpacity, View } from "react-native"
+
 
 const Profile = () => {
-  const [showEditable, setShowEditable] = useState(false)
+  const { signOut, userId } = useAuth();
+  const [isModalEditVisible, setIsModalEditVisible] = useState(false);
+  const currentUser = useQuery(api.users.getUserByClerk, userId ? { clerkId: userId } : "skip");
 
+  const [editedProfile, setEditedProfile] = useState({
+    fullname: currentUser?.fullname,
+    bio: currentUser?.bio
+  })
 
-  const { user } = useUser()
-  const currentUser = useQuery(api.users.getUserByClerk, user ? { clerkId: user.id } : "skip")
-  if (user == null) return <Loader />
+  const [seletectedPost, setSelectedPost] = useState<Doc<"posts"> | null>(null);
+  const posts = useQuery(api.posts.getPostbyUser, {})
 
+  const handleUpdateProfile = () => {
 
-  // console.log("Current User", currentUser)
+  }
 
+  if (!currentUser || posts === undefined) return <Loader />
 
   return (
-    <View className='flex-1 bg-black'>
-      {/* <Text className='text-white'>This is Profile Screen</Text>
-      <Text className='text-white'>{user?.fullName}</Text> */}
-
+    <View style={styles.container}>
       {/* Header */}
-      <View
-        style={{
-          backgroundColor: COLORS.background,
-          padding: 10,
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-between'
-        }}
-      >
-        <Text className='text-white font-bold text-xl'>{user.fullName}</Text>
-        <Ionicons name='log-out-outline' size={28} color={COLORS.primary} />
-      </View>
-
-      {/* user Information */}
-      <View className='mt-10 flex-row'>
-        <Image source={currentUser?.image} style={{ width: 100, height: 100, borderRadius: 50 }} contentFit='cover' transition={200} />
-        <View className='flex-row items-center justify-around flex-1'>
-          <View className='flex items-center'>
-            <Text className='text-white'>{currentUser?.posts}</Text>
-            <Text className='text-white'>Posts</Text>
-          </View>
-
-          <View className='flex items-center'>
-            <Text className='text-white'>{currentUser?.followers}</Text>
-            <Text className='text-white'>Followers</Text>
-          </View>
-
-          <View className='flex items-center'>
-            <Text className='text-white'>{currentUser?.following}</Text>
-            <Text className='text-white'>Following</Text>
-          </View>
-
+      <View style={styles.header}>
+        <View style={styles.headerLeft}>
+          <Text style={styles.username}>{currentUser.username}</Text>
+        </View>
+        <View style={styles.headerRight}>
+          <TouchableOpacity>
+            <Ionicons name="log-out-outline" size={24} color={COLORS.white} />
+          </TouchableOpacity>
         </View>
       </View>
 
-      <View className='mt-10'>
-        <Text className='text-white'>{currentUser?.fullname}</Text>
-        <Text className='text-white'>
-          {currentUser?.bio}
-        </Text>
-      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.profileInfo}>
+          {/* Avatar */}
+          <View style={styles.avatarAndStats}>
+            <View style={styles.avatarContainer}>
+              <Image
+                source={currentUser.image}
+                style={styles.avatar}
+                contentFit="cover"
+                transition={200}
+              />
+            </View>
+            <View style={styles.statsContainer}>
+              <View style={styles.statItem}>
+                <Text style={styles.statNumber}>{currentUser.posts}</Text>
+                <Text style={styles.statLabel}>Posts</Text>
+              </View>
 
-      {/* Edit profile section */}
-      <View>
-        <TouchableOpacity onPress={() => setShowEditable(true)} style={{ backgroundColor: COLORS.primary, width: "50%", padding: 10, borderRadius: 5 }}>
-          <Text className='text-center'>Edit Profile</Text>
-        </TouchableOpacity>
-      </View>
+              <View style={styles.statItem}>
+                <Text style={styles.statNumber}>{currentUser.followers}</Text>
+                <Text style={styles.statLabel}>Followers</Text>
+              </View>
 
-      {
-        showEditable && (
-          <Modal
-            animationType='slide'
-            style={{ flex: 1, backgroundColor: COLORS.background }}>
-            <View className='flex-1 bg-black'>
-              {/* Header */}
-              <View className='flex-row items-center justify-between px-8 py-4'>
-                <Text className='text-white'>Edit Profile</Text>
-                <Ionicons name='close' size={32} color={COLORS.primary} />
+              <View style={styles.statItem}>
+                <Text style={styles.statNumber}>{currentUser.following}</Text>
+                <Text style={styles.statLabel}>Following</Text>
               </View>
             </View>
-          </Modal>
-        )
-      }
-    </View>
+          </View>
 
+          {/* Full name and bio */}
+          <Text style={styles.name}>{currentUser.fullname}</Text>
+          {currentUser.bio && <Text style={styles.bio}>{currentUser.bio}</Text>}
+
+          {/* Edit option */}
+          <View style={styles.actionButtons}>
+            <TouchableOpacity style={styles.editButton} onPress={() => setIsModalEditVisible(true)}>
+              <Text style={styles.editButtonText}>Edit Profile</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.shareButton}>
+              <Ionicons name="share-outline" size={20} color={COLORS.white} />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        {posts.length == 0 && <NoPostFound /> }
+
+        {/* showing the images or posts */}
+        <FlatList
+        data={posts}
+        numColumns={3}
+        scrollEnabled={false}
+        renderItem={({ item }) => (
+          <TouchableOpacity style={styles.gridItem} onPress={() => setSelectedPost(item)}>
+            <Image
+            source={item.imageUrl}
+            style={styles.gridImage}
+            contentFit="cover"
+            transition={200}
+             />
+          </TouchableOpacity>
+        )}
+         />
+      </ScrollView>
+
+
+      {/* Edit Profile modal  */}
+      {/* Selected Image Modal */}
+      <Modal
+      visible={!!seletectedPost}
+      animationType="fade"
+      transparent={true}
+      onRequestClose={() => setSelectedPost(null)}
+      >
+        <View style={styles.modalBackdrop}>
+          {
+            seletectedPost && (
+              <View style={styles.postDetailContainer}>
+                <View style={styles.postDetailHeader}>
+                  <TouchableOpacity onPress={() => setSelectedPost(null)}>
+                    <Ionicons name="close" size={24} color={COLORS.white} />
+                  </TouchableOpacity>
+                </View>
+
+                <Image
+                source={seletectedPost.imageUrl}
+                style={styles.postDetailImage}
+                cachePolicy={"memory-disk"}
+                 />
+
+              </View>
+            )
+          }
+        </View>
+      </Modal>
+    </View>
   )
 }
 
 export default Profile
+
+
+const NoPostFound = () => {
+  return(
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl font-bold">No Posts Yet</Text>
+    </View>
+  )
+}
