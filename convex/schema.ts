e8fbe31ba8cc13e55a548b775109efb882e3d176
Vikash Thakur <thakvikash622@gmail.com>
2025-03-12 13:22:45 +0530

    Created: isFollowing Query

--- convex/schema.ts
+++ convex/schema.ts
@@ -38,7 +38,7 @@ export default defineSchema({
     follows: defineTable({
         followerId: v.id("users"),
         followingId: v.id("users"),
-    }).index("by_follower", ["followerId"]).index("by_following", ["followingId"]).index("both", ["followerId", "followingId"]),
+    }).index("by_follower", ["followerId"]).index("by_following", ["followingId"]).index("by_both", ["followerId", "followingId"]),
 
 
     notifications: defineTable({
